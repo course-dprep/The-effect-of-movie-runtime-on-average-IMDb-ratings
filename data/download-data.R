ee46444f10b2509c5a8b902283bf9cc88aaf5cc0
Demi Verburg <d.verburg@tilburguniversity.edu>
2026-02-19 14:16:39 +0100

    Enhance comments and directory creation in download script
    Updated comments to clarify the purpose of the script and added a check for the existence of the data directory.

--- data/download-data.R
+++ data/download-data.R
@@ -1,2 +1,20 @@
 # This script will be used to populate the \data directory
 # with all necessary raw data files.
+
+# This script populates the /data directory with the required IMDb raw files.
+
+dir.create("data", showWarnings = FALSE, recursive = TRUE)
+
+# 1) Ratings dataset
+url_ratings  <- "https://datasets.imdbws.com/title.ratings.tsv.gz"
+dest_ratings <- "data/title.ratings.tsv.gz"
+if (!file.exists(dest_ratings)) {
+  download.file(url_ratings, destfile = dest_ratings, mode = "wb")
+}
+
+# 2) Basics dataset
+url_basics  <- "https://datasets.imdbws.com/title.basics.tsv.gz"
+dest_basics <- "data/title.basics.tsv.gz"
+if (!file.exists(dest_basics)) {
+  download.file(url_basics, destfile = dest_basics, mode = "wb")
+}
