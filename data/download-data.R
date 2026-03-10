52b709432c13994addee6f32951e9cf45ff6da87
demiverburg <d.verburg@tilburguniversity.edu>
2026-03-10 13:54:51 +0100

    Updated download-data file

--- data/download-data.R
+++ data/download-data.R
@@ -1,20 +1,34 @@
 # This script will be used to populate the \data directory
 # with all necessary raw data files.
 
-# This script populates the /data directory with the required IMDb raw files.
+# This script populates the /data directory with the required IMDb raw files of title ratings and title basics.
 
-dir.create("data", showWarnings = FALSE, recursive = TRUE)
+library(readr)
+library(dplyr)
+library(tidyr)
 
-# 1) Ratings dataset
+# ---------------------------------------------------------
+# 1. Download and import ratings dataset
+#    Contains: tconst, averageRating, numVotes
+# ---------------------------------------------------------
+
+```{r}
 url_ratings  <- "https://datasets.imdbws.com/title.ratings.tsv.gz"
 dest_ratings <- "data/title.ratings.tsv.gz"
-if (!file.exists(dest_ratings)) {
-  download.file(url_ratings, destfile = dest_ratings, mode = "wb")
-}
+download.file(url_ratings, destfile = dest_ratings, mode = "wb")
 
-# 2) Basics dataset
+ratings <- read_tsv(dest_ratings, show_col_types = FALSE)
+
+```
+
+# ---------------------------------------------------------
+# 2. Download and import basics dataset
+# ---------------------------------------------------------
+
+```{r}
 url_basics  <- "https://datasets.imdbws.com/title.basics.tsv.gz"
 dest_basics <- "data/title.basics.tsv.gz"
-if (!file.exists(dest_basics)) {
-  download.file(url_basics, destfile = dest_basics, mode = "wb")
-}
+download.file(url_basics, destfile = dest_basics, mode = "wb")
+
+basics <- read_tsv(dest_basics, na = "\\N", show_col_types = FALSE)
+``
