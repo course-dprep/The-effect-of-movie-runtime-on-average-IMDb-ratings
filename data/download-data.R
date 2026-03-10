4438000d93293c3d62eafd2883a91ce12df846d2
Demi Verburg <d.verburg@tilburguniversity.edu>
2026-03-10 14:54:46 +0100

    Clean up R code chunk formatting in download-data.R
    Removed R code chunk formatting from download-data.R.

--- data/download-data.R
+++ data/download-data.R
@@ -12,20 +12,16 @@ library(tidyr)
 #    Contains: tconst, averageRating, numVotes
 # ---------------------------------------------------------
 
-```{r}
 url_ratings  <- "https://datasets.imdbws.com/title.ratings.tsv.gz"
 dest_ratings <- "data/title.ratings.tsv.gz"
 download.file(url_ratings, destfile = dest_ratings, mode = "wb")
 
 ratings <- read_tsv(dest_ratings, show_col_types = FALSE)
 
-```
-
 # ---------------------------------------------------------
 # 2. Download and import basics dataset
 # ---------------------------------------------------------
 
-```{r}
 url_basics  <- "https://datasets.imdbws.com/title.basics.tsv.gz"
 dest_basics <- "data/title.basics.tsv.gz"
 download.file(url_basics, destfile = dest_basics, mode = "wb")
